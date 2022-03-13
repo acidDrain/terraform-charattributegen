b629fd9f0b724f11e9fff06a7864e15b7ac08e81
James Grow <jmgrow@gmail.com>
2022-03-12 20:44:50 -0600

    Removed trailing forward slash from id provider url

--- iam.tf
+++ iam.tf
@@ -89,7 +89,7 @@ resource "aws_iam_role_policy_attachment" "cloudfront-attach" {
 }
 
 resource "aws_iam_openid_connect_provider" "github" {
-  url = "https://token.actions.githubusercontent.com/"
+  url = "https://token.actions.githubusercontent.com"
 
   client_id_list = [
     "sts.amazonaws.com",
